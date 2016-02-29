b12f816be625f0f968ce30d621834c87633bd0af
Vikram Swamy <vswamy@gmail.com>
2016-02-29 16:54:00 -0500

    added bootswatch files, layout views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user_login
+    if !logged_in?
+      flash[:danger] = "You are not logged in."
+      redirect_to root_path
+    end
+  end
+
+  def access_denied
+    flash[:danger] = "You are not permitted to do that."
+    redirect_to root_path
+  end
 end
